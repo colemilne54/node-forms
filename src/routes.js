869cbe0d0bb822d646366e30ff86649670ba4661
Cole Milne <colemilne54@gmail.com>
2021-07-30 14:30:51 -0600

    to date

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,42 @@
 const express = require('express');
 const router = express.Router();
 
+const multer = require('multer');
+const upload = multer({ storage: multer.diskStorage() });
+
 router.get('/', (req, res) => {
   res.render('index');
+});
+
+router.get('/contact', (req, res) => {
+  res.render('contact', {
+    data: {},
+    errors: {}
+  });
+});
+
+router.post('/contact', upload.single('photo'), (req, res) => {
+  
+  res.render('contact', {
+    data: req.body, // { message, email }
+    errors: {
+      message: {
+        msg: 'A message is required'
+      },
+      email: {
+        msg: 'That email doesn‘t look right'
+      }
+    }
+  });
+
+  if (req.file) {
+    console.log('Uploaded: ', req.file);
+  }
+
+  else 
+
+  console.log(req.body);
+  console.log(res.body);
 });
 
 module.exports = router;
